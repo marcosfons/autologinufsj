abaea859ebf7f79780902d8aa7580513bc51387d
Marcos Matins de sa Fonseca <192050013@lab-geral-02-09.dcomp>
2019-06-04 14:39:38 -0300

    Finalizado arquivo de configuração

--- setup.py
+++ setup.py
@@ -1,5 +1,3 @@
-
-
 import pip
 import os
 
@@ -14,15 +12,18 @@ def addToPath():
     home = expanduser("~")
     pasta = os.getcwd()
     os.chdir(home)
-    path = ''
     with open('.bashrc', 'a') as file:
         file.write('\nexport PATH="$PATH:' + pasta + '/"')
-# Example
 
 def createExecutable():
 	os.system('chmod +x autologin.py')
 
 if __name__ == '__main__':
-    # install('beautifulsoup4')
+		install('beautifulsoup4')
 		createExecutable()
 		addToPath()
+		print('\n' + ('-' * 54))
+		print('\n Instalação finalizada com êxito!')
+		print(' Rode o programa abrindo um novo terminal e digitando:')
+		print('\tautologin.py')
+		print('\n' + ('-' * 54))
