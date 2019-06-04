541357e424e28b5fcb6302c895591b215eb51f69
Marcos Matins de sa Fonseca <192050013@lab-geral-02-07.dcomp>
2019-06-04 13:10:58 -0300

    Criado arquivo de setup

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 
 
 import pip
+import os
 
 def install(package):
     if hasattr(pip, 'main'):
@@ -8,6 +9,20 @@ def install(package):
     else:
         pip._internal.main(['install', package])
 
+def addToPath():
+    from os.path import expanduser
+    home = expanduser("~")
+    pasta = os.getcwd()
+    os.chdir(home)
+    path = ''
+    with open('.bashrc', 'a') as file:
+        file.write('\nexport PATH="$PATH:' + pasta + '/"')
 # Example
+
+def createExecutable():
+	os.system('chmod +x autologin.py')
+
 if __name__ == '__main__':
-    install('beautifulsoup4')
+    # install('beautifulsoup4')
+		createExecutable()
+		addToPath()
---
+++ setup.sh
@@ -0,0 +1,11 @@
+# Creditos a Carlos Magno
+# Github: https://github.com/cmagnobarbosa
+# Parte do setup, foi inspirada em seu codigo
+
+if[ $verifica = 0]
+then
+	echo \# Configuracao do autologin wifi UFSJ >> ~/.bashrc
+	echo alias logar=\'python3 /opt/autologinufsj/autologin.py\' >> ~/.bashrc
+fi
+
+
